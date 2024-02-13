c298658d5db7425d861d40f04c543de10778ea3c
dvtruong-dot <derek_truong@brown.edu>
2024-02-13 16:45:46 -0500

    pair-prog w/ @slopezlu: made CSVDataSource also house the headers and return a proxy if the method is called

--- src/main/java/edu/brown/cs/student/main/server/CSVAPI/CSVDataSource.java
+++ src/main/java/edu/brown/cs/student/main/server/CSVAPI/CSVDataSource.java
@@ -3,36 +3,65 @@ package edu.brown.cs.student.main.server.CSVAPI;
 import edu.brown.cs.student.main.Parser.CSVParser;
 import edu.brown.cs.student.main.Parser.Creator.ExampleCreators.ListofStringCreator;
 import edu.brown.cs.student.main.Parser.Creator.FactoryFailureException;
-
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.regex.Pattern;
 
 public class CSVDataSource {
-    private List<List<String>> csv;
+  private List<List<String>> csv;
+  private List<List<String>> publicCSV;
+  private HashMap<String, Integer> headers;
 
-    public CSVDataSource() {
-        this.csv = null; //not sure if using null in this case is smart
+  public CSVDataSource() {
+    this.csv = null; // not sure if using null in this case is smart
+  }
+
+  public void load(String filename, boolean headersOrNot)
+      throws IOException, FactoryFailureException {
+    // use parser to parse the desired file and fill csv with it
+    // also figure out how to make it safer (they can only access a certain directory or something
+    // like that)
+    // ^^^this may have to be done in the handler
+    final Pattern regexSplitCSVRow =
+        Pattern.compile(",(?=([^\\\"]*\\\"[^\\\"]*\\\")*(?![^\\\"]*\\\"))");
+    BufferedReader reader = new BufferedReader(new FileReader(filename));
+    String firstRow = reader.readLine();
+    String[] rowArray = regexSplitCSVRow.split(firstRow);
+
+    //for the headers
+    this.headers = new HashMap<>();
+    if (headersOrNot) {
+      int i = 0;
+      for (String item: rowArray) {
+        this.headers.put(item, i);
+        i++;
+      }
+    } else {
+      int i = 0;
+      for (String item: rowArray) {
+        this.headers.put(Integer.toString(i), i);
+        i++;
+      }
     }
 
-    public void load(String filename, boolean headersOrNot) throws IOException, FactoryFailureException {
-        //use parser to parse the desired file and fill csv with it
-        //also figure out how to make it safer (they can only access a certain directory or something like that)
-        //^^^this may have to be done in the handler
-        final Pattern regexSplitCSVRow =
-                Pattern.compile(",(?=([^\\\"]*\\\"[^\\\"]*\\\")*(?![^\\\"]*\\\"))");
-        BufferedReader reader = new BufferedReader(new FileReader(filename));
-        String firstRow = reader.readLine();
-        String[] rowArray = regexSplitCSVRow.split(firstRow);
+    this.csv =
+        new CSVParser<List<String>>(
+                new FileReader(filename), new ListofStringCreator(rowArray.length), headersOrNot)
+            .parse();
+  }
 
-        this.csv = new CSVParser<List<String>>(new FileReader(filename), new ListofStringCreator(rowArray.length), headersOrNot).parse();
-    }
+  public boolean checkLoaded() {
+    return this.csv != null;
+  }
 
-    public boolean checkLoaded() {
-        return this.csv != null;
+  public List<List<String>> proxy() {
+    if (publicCSV == null) {
+      this.publicCSV = Collections.unmodifiableList(this.csv);
     }
+    return publicCSV;
+  }
 }
---
+++ src/main/java/edu/brown/cs/student/main/server/CSVAPI/ViewCSVHandler.java
@@ -0,0 +1,2 @@
+package edu.brown.cs.student.main.server.CSVAPI;public class ViewCSVHandler {
+}
